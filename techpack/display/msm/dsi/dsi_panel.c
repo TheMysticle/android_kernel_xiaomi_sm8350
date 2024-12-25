c6fcb382a09473da5a7d0e508e01eb2cf81dab7f
Rocky7842 <eric.rocky7842@gmail.com>
2024-12-25 22:51:14 +0800

    techpack: display: dsi: Use hbm_bl_max_level for for local HBM reg during HBM
    * Otherwise, the non-fp area will be dimmed using incorrect brightness value.
    
    Change-Id: I248b3ee4d610a65ead57e4f95153f7d29859e5aa

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -695,7 +695,8 @@ static int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status)
 		} else {
 			rc = dsi_panel_update_cmd_reg51(panel,
 							DSI_CMD_SET_MI_LOCAL_HBM_NORMAL_WHITE_1000NIT,
-							panel->bl_config.real_bl_level);
+							panel->hbm_mode ? panel->bl_config.hbm_bl_max_level
+									: panel->bl_config.real_bl_level);
 			rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_MI_LOCAL_HBM_NORMAL_WHITE_1000NIT);
 			if (rc)
 				DSI_ERR("[%s] failed to send local hbm on cmd, rc=%d\n",
