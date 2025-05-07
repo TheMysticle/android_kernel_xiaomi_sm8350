5316f805ef1c0d44c043a5eddc37f5e6dc233b6b
Rocky7842 <eric.rocky7842@gmail.com>
2025-05-07 19:44:47 +0800

    techpack: display: dsi: Switch back to generic doze commands handling
    This reverts the follow commits:
    c432a45edf5c187a8a11d45c295ec3ec9fc53d24
    b372457b482f8cda3e98f965b30bba648d522255

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -205,12 +205,6 @@ struct dsi_panel_ops {
 	int (*parse_power_cfg)(struct dsi_panel *panel);
 };
 
-enum dsi_doze_mode_type {
-	DSI_DOZE_MODE_NOLP,
-	DSI_DOZE_MODE_LP_LBM,
-	DSI_DOZE_MODE_LP_HBM,
-};
-
 struct dsi_panel {
 	const char *name;
 	const char *type;
@@ -276,10 +270,8 @@ struct dsi_panel {
 	u32 tlmm_gpio_count;
 
 	struct dsi_panel_ops panel_ops;
-
-	enum dsi_doze_mode_type doze_mode_active;
-	enum dsi_doze_mode_type doze_mode_requested;
-	bool aod_nolp_command_enabled;
+	
+	bool doze_enabled;
 
 	bool fod_hbm_enabled;
 	bool fod_hbm_requested;
